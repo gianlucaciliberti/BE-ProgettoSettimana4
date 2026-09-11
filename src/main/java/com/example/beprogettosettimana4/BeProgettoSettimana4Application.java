e39804e9c2cd4c98c34884a58d34efd6b4c58625
gianlucaciliberti <gianlu.cili.gc@gmail.com>
2026-09-11 12:57:37 +0200

    base registrazione utenti

---
+++ src/main/java/com/example/beprogettosettimana4/BeProgettoSettimana4Application.java
@@ -0,0 +1,13 @@
+package com.example.beprogettosettimana4;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BeProgettoSettimana4Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BeProgettoSettimana4Application.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/beprogettosettimana4/BeProgettoSettimana4ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.beprogettosettimana4;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BeProgettoSettimana4ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
